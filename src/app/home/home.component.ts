18bd21de765fd0483042b90886034534ac225a51
Trupti Deshmukh <truptideshmukh@opticalarc.com>
2024-04-23 14:55:10 +0530

    Trendy products style and functionality added.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,19 @@ import { product } from '../datatype';
 })
 export class HomeComponent {
 popularProducts: undefined | product[]
+trendyProducts: undefined |product[]
   constructor(private product:ProductService){}
   ngOnInit(){
     this.product.popularProducts().subscribe((result)=>{
       if(Array.isArray(result)){   // Array.isArray is JS method to check the result is array or not. if it array then it is stored in productlist
         this.popularProducts =result;
       }
-      console.log(this.popularProducts)
+     // console.log(this.popularProducts)
+    })
+    this.product.trendyProducts().subscribe((result)=>{
+    if(Array.isArray(result)){
+      this.trendyProducts=result;
+    }
     })
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -29,5 +29,7 @@ export class ProductService {
   popularProducts(){
     return this.http.get<product>('http://localhost:3000/products?_limit=3');
   }
-
+  trendyProducts(){
+    return this.http.get<product>('http://localhost:3000/products?_limit=8');
+  }
 }
